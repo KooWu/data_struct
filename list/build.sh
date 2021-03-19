cd30e1f07f53d870fe4c494e24b49b424700b35e
Your Name <you@example.com>
2021-03-19 22:57:20 +0800

    add sort&swap api & build.sh

---
+++ list/build.sh
@@ -0,0 +1,1 @@
+gcc list.c list_stack.c list_queue.c -o list
--- list/list.c
+++ list/list.c
@@ -92,13 +92,75 @@ int DelHeadListNode(ListNode *root, ListNode *node)
     return 0;
 }
 
+static void SwapListNextNode(ListNode *node1, ListNode *node2)
+{
+    ListNode *node1Prev = node1->prev;
+    ListNode *node2Next = node2->next;
+
+    node1->next = node2Next;
+    node2Next->prev = node1;
+
+    node1Prev->next = node2;
+    node2->next = node1;
+    node1->prev = node2;
+    node2->prev = node1Prev;  
+}
+
+void SwapListAdjacentNode(ListNode *node1, ListNode *node2)
+{
+    if (node1->next == node2) {
+        SwapListNextNode(node1, node2);
+    } else {
+        SwapListNextNode(node2, node1);
+    }  
+}
+
+void SwapListNode(ListNode *node1, ListNode *node2)
+{
+    if (node1->next == node2 || node1->prev == node2) {
+        return SwapListAdjacentNode(node1, node2);
+    }
+
+    ListNode *node1Prev = node1->prev;
+    ListNode *node1Next = node1->next;
+    ListNode *node2Prev = node2->prev;
+    ListNode *node2Next = node2->next;
+
+    node1Prev->next = node2;
+    node2->next = node1Next;
+    node1Next->prev = node2;
+    node2->prev = node1Prev;
+
+    node2Prev->next = node1;
+    node1->next = node2Next;
+    node2Next->prev = node1;
+    node1->prev = node2Prev;
+}
+
+void SortListNode(ListNode *root)
+{
+    ListNode *cur = root;
+    ListNode *end = root;
+    ListNode *p = NULL;
+    for (; cur != root->prev; end = end->prev) {
+        for (p = root->next; p != end->prev;) {
+            if (p->val > p->next->val) {
+                SwapListAdjacentNode(p, p->next);
+            } else {
+                p = p->next;
+            }
+        }
+        cur = cur->next;
+    }
+}
+
 void PrintListNode(ListNode *root)
 {
     ListNode *node = root->next;
     for (; node != root; node = node->next) {
         printf("%d->", node->val);
     }
-    printf("\n");
+    printf("end\n");
 }
 
 void ReleaseListNode(ListNode *root)
@@ -121,26 +183,33 @@ void TestListNode(void)
     ListNode *node = CreateListNode(2);
     InsertHeadListNode(&root, node);
 
-    node = CreateListNode(3);
-    InsertHeadListNode(&root, node);
+    ListNode *node1 = CreateListNode(13);
+    InsertHeadListNode(&root, node1);
 
-    node = CreateListNode(4);
-    InsertHeadListNode(&root, node);
+    ListNode *node2 = CreateListNode(4);
+    InsertHeadListNode(&root, node2);
 
-    node = CreateListNode(1);
+    ListNode *node3 = CreateListNode(1);
+    InsertHeadListNode(&root, node3);
+
+    PrintListNode(&root);
+    SwapListNode(node3, node1);
+    PrintListNode(&root);
+
+    node = CreateListNode(5);
     InsertTailListNode(&root, node);
 
     node = CreateListNode(11);
     InsertTailListNode(&root, node);
-
+    SortListNode(&root);
     PrintListNode(&root);
     ReleaseListNode(&root);
 }
 
 int main(void)
 {
-    //TestListNode();
+    TestListNode();
     //TestNodeQueue();
-    TestNodeStack();
+    //TestNodeStack();
     return 0;
 }
